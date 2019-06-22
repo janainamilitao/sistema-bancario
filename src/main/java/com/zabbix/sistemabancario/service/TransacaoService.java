b76278a77f26b9c613c2f9831c7c5b4c74c436e2
Janaina <janaina.militao@gmail.com>
2019-06-22 12:18:37 -0300

    Refatoração e ajustes no saque e depósito

--- src/main/java/com/zabbix/sistemabancario/service/TransacaoService.java
+++ src/main/java/com/zabbix/sistemabancario/service/TransacaoService.java
@@ -1,30 +1,166 @@
 package com.zabbix.sistemabancario.service;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.zabbix.sistemabancario.exception.ContaDisabledException;
+import com.zabbix.sistemabancario.exception.ContaNotFoundException;
 import com.zabbix.sistemabancario.model.Conta;
+import com.zabbix.sistemabancario.model.Extrato;
+import com.zabbix.sistemabancario.model.OperacaoFinanceira;
+import com.zabbix.sistemabancario.model.TipoTransacaoEnum;
 import com.zabbix.sistemabancario.model.Transacao;
+import com.zabbix.sistemabancario.repository.ContaRepository;
 import com.zabbix.sistemabancario.repository.TransacaoRepository;
+import com.zabbix.sistemabancario.utils.DateUtil;
 /**
  * @author janaina militão
  */
 
 @Service
 public class TransacaoService extends GenericService<Transacao>{
-	
-	@Autowired
-    private TransacaoRepository repository;	  
 
-	public List<Transacao> findByConta(Conta conta) {
-        return repository.findByConta(conta);
-    }
+	@Autowired
+	private TransacaoRepository transacaoRepository;
+
+	@Autowired
+	private ContaRepository contaReporitory;
+
+	public Conta deposito(BigDecimal valor, Long idConta) throws ContaNotFoundException, ContaDisabledException {
+		Optional<Conta> contaOptional = contaReporitory.findById(idConta);		
+
+		if(contaOptional.isPresent()) {	
+			Conta conta = contaOptional.get();
+
+			if(conta.getFlagAtivo().equals(false)) {
+				throw new ContaDisabledException();
+			}
+
+			Transacao transacao = new Transacao();
+
+
+			transacao.setDataTransacao(new Date());
+			transacao.setConta(conta);
+			transacao.setTipoTransacao(TipoTransacaoEnum.DEPOSITO);
+			transacao.setValor(valor);
+			transacaoRepository.saveAndFlush(transacao);
+
+			conta.setSaldo(conta.getSaldo().add(transacao.getValor()));				
+			return contaReporitory.saveAndFlush(conta);
+		}else {			
+			throw new ContaNotFoundException();
+		}
+	}
+
+	public Conta saque(BigDecimal valor, Long idConta) throws Exception {
+
+		Optional<Conta> contaOptional = contaReporitory.findById(idConta);		
+
+		if(contaOptional.isPresent()) {	
+			Conta conta = contaOptional.get();
+
+			if(conta.getFlagAtivo().equals(false)) {
+				throw new ContaDisabledException();
+			}
+
+			if(valor.longValueExact()>conta.getSaldo().longValueExact()) {
+				throw new Exception("Saldo insuficiente para o saque. Verique o saldo da conta.");
+			}else {
+				if(valor.longValue()>conta.getLimiteSaqueDiario().longValue()) {
+					throw new Exception("O saque excede o limite de saque diário de R$"+conta.getLimiteSaqueDiario());
+				}
+
+				Transacao transacao = new Transacao();				
+				transacao.setDataTransacao(new Date());
+				transacao.setConta(conta);
+				transacao.setTipoTransacao(TipoTransacaoEnum.SAQUE);
+				transacao.setValor(valor);
+				transacaoRepository.saveAndFlush(transacao);
+
+				conta.setSaldo(conta.getSaldo().subtract(transacao.getValor()));				
+				return contaReporitory.saveAndFlush(conta);
+			}	
+
+		}else {			
+			throw new ContaNotFoundException();
+		}
+	}
+
+	public Extrato extrato(Long idConta) throws ContaNotFoundException {
+		Optional<Conta> contaOptional = contaReporitory.findById(idConta);		
+
+		if(contaOptional.isPresent()) {	
+			Conta conta = contaOptional.get();	
+			List<Transacao> transacoes = transacaoRepository.findByConta(conta);
+
+			Extrato extrato = calcularExtrato(conta, transacoes);
+
+			return extrato;
+
+		}else {			
+			throw new ContaNotFoundException();
+		}
+	}	
+
+	public Extrato extratoPorPeriodo(Long idConta, int mes, int ano) throws ContaNotFoundException {
+		Optional<Conta> contaOptional = contaReporitory.findById(idConta);	
+
+		if(contaOptional.isPresent()) {	
+			Conta conta = contaOptional.get();
+
+			Date inicioPeriodo = DateUtil.retornarDataInicio(mes -1 , ano);
+			Date fimPeriodo = DateUtil.retornarDataFim(mes -1, ano);
+
+			List<Transacao> transacoes = transacaoRepository.findByContaAndDataTransacaoBetween(conta, inicioPeriodo, fimPeriodo);
+
+			Extrato extrato = calcularExtrato(conta, transacoes);
+
+			return extrato;
+		}else {
+			throw new ContaNotFoundException();
+		}		
+	}	
 	
-	public List<Transacao> findByContaAndDataTransacaoBetween(Conta conta, Date inicio, Date fim) {
-        return repository.findByContaAndDataTransacaoBetween(conta, inicio, fim);
-    }
+	private Extrato calcularExtrato(Conta conta, List<Transacao> transacoes) {
+		Extrato extrato = new Extrato();
+		extrato.setIdConta(conta.getId());
+		extrato.setUsuarioConta(conta.getPessoa().getNome());
+
+
+		List<OperacaoFinanceira> operacoes = new ArrayList<OperacaoFinanceira>();
+		for(Transacao transacao: transacoes) {
+			OperacaoFinanceira operacao = new OperacaoFinanceira();
+			operacao.setDataOperacao(transacao.getDataTransacao());
+			operacao.setTipoOperacao(transacao.getTipoTransacao());	
+			operacao.setValorOperacao(transacao.getValor());
+
+			BigDecimal saldoAnterior = conta.getSaldo();
+
+			if(operacao.getTipoOperacao().equals(TipoTransacaoEnum.DEPOSITO)) {
+				saldoAnterior = saldoAnterior.subtract(transacao.getValor());
+				operacao.setSaldoAnterior(saldoAnterior);
+				operacao.setSaldoAtual(saldoAnterior.add(transacao.getValor()));
+
+			}else if(operacao.getTipoOperacao().equals(TipoTransacaoEnum.SAQUE)) {
+				saldoAnterior = saldoAnterior.add(transacao.getValor());
+				operacao.setSaldoAnterior(saldoAnterior);
+				operacao.setSaldoAtual(saldoAnterior.subtract(transacao.getValor()));				
+			}
+
+			operacoes.add(operacao);
+		}
+
+		extrato.setOperacaoFinanceiraList(operacoes);
+
+		return extrato;
+	}
+
+	
 
 }
